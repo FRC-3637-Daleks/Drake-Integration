5698d3d4e26b1785a503d6e9288c5573e7f0ada7
Dave.Lawrence <davemlnj@gmail.com>
2019-01-18 22:53:11 -0500

    Working single class model

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -8,8 +8,6 @@
 #include "Robot.h"
 #include "frc/SerialPort.h"
 #include "frc/AnalogPotentiometer.h"
-#include "vl53l0x_api.h"
-#include "vl53l0x_platform.h"
 #include "frc/DigitalOutput.h"
 
 #include <iostream>
@@ -19,244 +17,9 @@
 frc::AnalogPotentiometer *pot;
 frc::AnalogPotentiometer *lineSensor;
 frc::DigitalOutput *led;
-
-#define VERSION_REQUIRED_MAJOR 1
-#define VERSION_REQUIRED_MINOR 0
-#define VERSION_REQUIRED_BUILD 1
-
-
-void print_pal_error(VL53L0X_Error Status){
-    char buf[VL53L0X_MAX_STRING_LENGTH];
-    VL53L0X_GetPalErrorString(Status, buf);
-    printf("API Status: %i : %s\n", Status, buf);
-}
-
-void print_range_status(VL53L0X_RangingMeasurementData_t* pRangingMeasurementData){
-    char buf[VL53L0X_MAX_STRING_LENGTH];
-    uint8_t RangeStatus;
-
-    /*
-     * New Range Status: data is valid when pRangingMeasurementData->RangeStatus = 0
-     */
-
-    RangeStatus = pRangingMeasurementData->RangeStatus;
-
-    VL53L0X_GetRangeStatusString(RangeStatus, buf);
-    printf("Range Status: %i : %s\n", RangeStatus, buf);
-
-}
-
-
-VL53L0X_Error rangingTest(VL53L0X_Dev_t *pMyDevice)
-{
-    VL53L0X_Error Status = VL53L0X_ERROR_NONE;
-    VL53L0X_RangingMeasurementData_t    RangingMeasurementData;
-    int i;
-    uint32_t refSpadCount;
-    uint8_t isApertureSpads;
-    uint8_t VhvSettings;
-    uint8_t PhaseCal;
-
-    if(Status == VL53L0X_ERROR_NONE)
-    {
-        printf ("Call of VL53L0X_StaticInit\n");
-        Status = VL53L0X_StaticInit(pMyDevice); // Device Initialization
-        print_pal_error(Status);
-    }
-    
-    if(Status == VL53L0X_ERROR_NONE)
-    {
-        printf ("Call of VL53L0X_PerformRefCalibration\n");
-        Status = VL53L0X_PerformRefCalibration(pMyDevice,
-        		&VhvSettings, &PhaseCal); // Device Initialization
-        print_pal_error(Status);
-    }
-
-    if(Status == VL53L0X_ERROR_NONE)
-    {
-        printf ("Call of VL53L0X_PerformRefSpadManagement\n");
-        Status = VL53L0X_PerformRefSpadManagement(pMyDevice,
-        		&refSpadCount, &isApertureSpads); // Device Initialization
-        printf ("refSpadCount = %d, isApertureSpads = %d\n", refSpadCount, isApertureSpads);
-        print_pal_error(Status);
-    }
-
-    if(Status == VL53L0X_ERROR_NONE)
-    {
-
-        // no need to do this when we use VL53L0X_PerformSingleRangingMeasurement
-        printf ("Call of VL53L0X_SetDeviceMode\n");
-        Status = VL53L0X_SetDeviceMode(pMyDevice, VL53L0X_DEVICEMODE_SINGLE_RANGING); // Setup in single ranging mode
-        print_pal_error(Status);
-    }
-
-    // Enable/Disable Sigma and Signal check
-	
- /*   if (Status == VL53L0X_ERROR_NONE) {
-        Status = VL53L0X_SetSequenceStepEnable(pMyDevice,VL53L0X_SEQUENCESTEP_DSS, 1);
-    }*/	
-	
-    if (Status == VL53L0X_ERROR_NONE) {
-        Status = VL53L0X_SetLimitCheckEnable(pMyDevice,
-        		VL53L0X_CHECKENABLE_SIGMA_FINAL_RANGE, 1);
-    }
-    if (Status == VL53L0X_ERROR_NONE) {
-        Status = VL53L0X_SetLimitCheckEnable(pMyDevice,
-        		VL53L0X_CHECKENABLE_SIGNAL_RATE_FINAL_RANGE, 1);
-    }
-				
-    if (Status == VL53L0X_ERROR_NONE) {
-        Status = VL53L0X_SetLimitCheckValue(pMyDevice,
-        		VL53L0X_CHECKENABLE_SIGNAL_RATE_FINAL_RANGE,
-        		(FixPoint1616_t)(0.1*65536));
-	}			
-    if (Status == VL53L0X_ERROR_NONE) {
-        Status = VL53L0X_SetLimitCheckValue(pMyDevice,
-        		VL53L0X_CHECKENABLE_SIGMA_FINAL_RANGE,
-        		(FixPoint1616_t)(60*65536));			
-    }
-    if (Status == VL53L0X_ERROR_NONE) {
-        Status = VL53L0X_SetMeasurementTimingBudgetMicroSeconds(pMyDevice,
-        		33000);
-	}
-	
-    if (Status == VL53L0X_ERROR_NONE) {
-        Status = VL53L0X_SetVcselPulsePeriod(pMyDevice, 
-		        VL53L0X_VCSEL_PERIOD_PRE_RANGE, 18);
-    }
-    if (Status == VL53L0X_ERROR_NONE) {
-        Status = VL53L0X_SetVcselPulsePeriod(pMyDevice, 
-		        VL53L0X_VCSEL_PERIOD_FINAL_RANGE, 14);
-    }
-
-
-    /*
-     *  Step  4 : Test ranging mode
-     */
-
-    if(Status == VL53L0X_ERROR_NONE)
-    {
-        for(i=0;i<50;i++){
-            printf ("Call of VL53L0X_PerformSingleRangingMeasurement\n");
-            Status = VL53L0X_PerformSingleRangingMeasurement(pMyDevice,
-            		&RangingMeasurementData);
-
-            print_pal_error(Status);
-            print_range_status(&RangingMeasurementData);
-
-           
-            if (Status != VL53L0X_ERROR_NONE) break;
-
-            printf("Measured distance: %i\n\n", RangingMeasurementData.RangeMilliMeter);
-
-
-        }
-    }
-    return Status;
-}
-
-int tmain(int argc, char **argv)
-{
-    VL53L0X_Error Status = VL53L0X_ERROR_NONE;
-    VL53L0X_Dev_t MyDevice;
-    VL53L0X_Dev_t *pMyDevice = &MyDevice;
-    VL53L0X_Version_t                   Version;
-    VL53L0X_Version_t                  *pVersion   = &Version;
-    VL53L0X_DeviceInfo_t                DeviceInfo;
-
-    int32_t status_int;
-    
-    printf ("VL53L0X API Simple Ranging example\n\n");
-
-    // Initialize Comms
-    pMyDevice->I2cDevAddr      = 0x29;
-
-    pMyDevice->fd = VL53L0X_i2c_init("/dev/i2c-1", pMyDevice->I2cDevAddr);//choose between i2c-0 and i2c-1; On the raspberry pi zero, i2c-1 are pins 2 and 3
-    if (MyDevice.fd<0) {
-        Status = VL53L0X_ERROR_CONTROL_INTERFACE;
-        printf ("Failed to init\n");
-    }
-
-    /*
-     *  Get the version of the VL53L0X API running in the firmware
-     */
-
-    if(Status == VL53L0X_ERROR_NONE)
-    {
-        status_int = VL53L0X_GetVersion(pVersion);
-        if (status_int != 0)
-            Status = VL53L0X_ERROR_CONTROL_INTERFACE;
-    }
-
-    /*
-     *  Verify the version of the VL53L0X API running in the firmware
-     */
-
-    if(Status == VL53L0X_ERROR_NONE)
-    {
-        if( pVersion->major != VERSION_REQUIRED_MAJOR ||
-            pVersion->minor != VERSION_REQUIRED_MINOR ||
-            pVersion->build != VERSION_REQUIRED_BUILD )
-        {
-            printf("VL53L0X API Version Error: Your firmware has %d.%d.%d (revision %d). This example requires %d.%d.%d.\n",
-                pVersion->major, pVersion->minor, pVersion->build, pVersion->revision,
-                VERSION_REQUIRED_MAJOR, VERSION_REQUIRED_MINOR, VERSION_REQUIRED_BUILD);
-        }
-    }
-
-
-    if(Status == VL53L0X_ERROR_NONE)
-    {
-        printf ("Call of VL53L0X_DataInit\n");
-        Status = VL53L0X_DataInit(&MyDevice); // Data initialization
-        print_pal_error(Status);
-    }
-
-    if(Status == VL53L0X_ERROR_NONE)
-    {
-        Status = VL53L0X_GetDeviceInfo(&MyDevice, &DeviceInfo);
-        if(Status == VL53L0X_ERROR_NONE)
-        {
-            printf("VL53L0X_GetDeviceInfo:\n");
-            printf("Device Name : %s\n", DeviceInfo.Name);
-            printf("Device Type : %s\n", DeviceInfo.Type);
-            printf("Device ID : %s\n", DeviceInfo.ProductId);
-            printf("ProductRevisionMajor : %d\n", DeviceInfo.ProductRevisionMajor);
-            printf("ProductRevisionMinor : %d\n", DeviceInfo.ProductRevisionMinor);
-
-        if ((DeviceInfo.ProductRevisionMinor != 1) && (DeviceInfo.ProductRevisionMinor != 1)) {
-        	printf("Error expected cut 1.1 but found cut %d.%d\n",
-                       DeviceInfo.ProductRevisionMajor, DeviceInfo.ProductRevisionMinor);
-                Status = VL53L0X_ERROR_NOT_SUPPORTED;
-            }
-        }
-        print_pal_error(Status);
-    }
-
-    if(Status == VL53L0X_ERROR_NONE)
-    {
-        Status = rangingTest(pMyDevice);
-    }
-
-    print_pal_error(Status);
-    
-    // Implementation specific
-
-    /*
-     *  Disconnect comms - part of VL53L0X_platform.c
-     */
-
-    printf ("Close Comms\n");
-    VL53L0X_i2c_close();
-
-    print_pal_error(Status);
-	
-
-    
-    return (0);
-}
-
-
+frc::DigitalOutput *led10;
+#define LIDAR_COUNT 8
+Sensor_VL53L0X *mLidar[LIDAR_COUNT];
 
 void Robot::RobotInit() {
   m_chooser.SetDefaultOption(kAutoNameDefault, kAutoNameDefault);
@@ -265,7 +28,18 @@ void Robot::RobotInit() {
 
   pot = new frc::AnalogPotentiometer(0, 300, 0);
   lineSensor = new frc::AnalogPotentiometer(1, 10, 0);
-  led = new frc::DigitalOutput(0);
+  led =   new frc::DigitalOutput(0);
+  led10 = new frc::DigitalOutput(11);
+  led10->Set(1);
+
+//  for(int i=0; i<LIDAR_COUNT; i++)
+//  {
+//    mLidar[i] = new Sensor_VL53L0X();
+//   mLidar[i]->Init(0x29+i);
+//  }
+  mLidar[0] = new Sensor_VL53L0X;
+  mLidar[0]->Init(0x29);
+    //Status = rangingTest(pMyDevice);
 
 }
 
@@ -278,9 +52,9 @@ void Robot::RobotInit() {
  * LiveWindow and SmartDashboard integrated updating.
  */
 void Robot::RobotPeriodic() {
-
+  static int state=0;
   //std::cout << "POT = " << pot->Get() << std::endl;
-  if (lineSensor->Get() < 1.6 && lineSensor->Get() > 1.58)
+/*  if (lineSensor->Get() < 1.6 && lineSensor->Get() > 1.58)
   {
     std::cout << "Line" << std::endl;
     led->Set(1);
@@ -294,8 +68,23 @@ void Robot::RobotPeriodic() {
   {
     std::cout << "No Line" << std::endl;
     led->Set(0);
-  }
+  }*/
+  if(state++>9) state=0;
+  led10->Set(state>4?1:0);
   //std::cout << "Line Sensor = " << lineSensor->Get() << std::endl;
+
+  std::cout << "Measurement= " << mLidar[0]->MeasureMM() << std::endl;
+/*  Status = VL53L0X_PerformSingleRangingMeasurement(pMyDevice,
+      &RangingMeasurementData);
+  if(RangingMeasurementData.RangeMilliMeter < 8000)
+    printf("Measured distance: %i\n", RangingMeasurementData.RangeMilliMeter);
+*/
+//    Status = rangingTest(pMyDevice);
+/*    if(Status == VL53L0X_ERROR_NONE)
+    {
+      std::cout << "Distance = " << Status << std::endl;
+    }
+*/
 }
 
 /**
