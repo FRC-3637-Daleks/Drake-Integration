4b956084f7d58c6fee821a8627843f17e779480d
Dave.Lawrence <davemlnj@gmail.com>
2019-01-26 11:11:09 -0500

    work in progress

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -11,29 +11,54 @@
 #include "frc/SerialPort.h"
 #include "frc/AnalogPotentiometer.h"
 #include "frc/DigitalOutput.h"
+#include "frc/DigitalInput.h"
 #include "frc/smartdashboard/SmartDashboard.h"
+#include "frc/shuffleboard/Shuffleboard.h"
+#include "frc/shuffleboard/ShuffleboardTab.h"
+//#include "frc/smartdashboard/SendableChooser.h"
+#include "frc/Servo.h"
+#include "frc/smartdashboard/SendableBuilder.h"
+#include "C:\FRC2019\Timed\Drake-Integration\build\tmp\expandedArchives\ntcore-cpp-2019.1.1-headers.zip_de8bb156e7c972a7498927ac56d827f7\networktables\NetworkTableEntry.h"
 
 #include "Helpers.h"
 #include "Helper_VL53L0X.h"
 
 frc::AnalogPotentiometer *pot;
-frc::AnalogPotentiometer *lineSensor;
+frc::DigitalInput *lineSensor;
 //frc::DigitalOutput *led;
 //frc::DigitalOutput *led10;
-#define LIDAR_COUNT 4
+#define LIDAR_COUNT 6
 Sensor_VL53L0X *mLidar[LIDAR_COUNT];
 frc::DigitalOutput *lidarSHDN[LIDAR_COUNT];
+frc::Shuffleboard *shuffleBoard;
+//frc::ShuffleboardTab *shuffleboardTab;
+//frc::SendableChooser *sendableChooser;
+frc::Servo *servo0;
+nt::NetworkTableEntry *servoNTE;
 
 void Robot::RobotInit() {
+  //sendableChooser->InitSendable();
+  //sendableChooser->AddOption("Pizza", "meow");
+  
   BlockingTimer Timer;
 
-  printf("\nRobo Init\n\n");
-  m_chooser.SetDefaultOption(kAutoNameDefault, kAutoNameDefault);
-  m_chooser.AddOption(kAutoNameCustom, kAutoNameCustom);
-  frc::SmartDashboard::PutData("Auto Modes", &m_chooser);
+  
 
-  pot = new frc::AnalogPotentiometer(0, 300, 0);
-  lineSensor = new frc::AnalogPotentiometer(1, 10, 0);
+  printf("\nRobo Init\n\n");
+  //m_chooser.SetDefaultOption(kAutoNameDefault, kAutoNameDefault);
+  //m_chooser.AddOption(kAutoNameCustom, kAutoNameCustom);
+  //frc::SmartDashboard::PutData("Auto Modes", &m_chooser);
+
+
+  lineSensor = new frc::DigitalInput(8);
+  servo0 = new frc::Servo(0);
+  pot = new frc::AnalogPotentiometer(0, servo0->GetMaxAngle() - servo0->GetMinAngle(), servo0->GetMinAngle());
+  servoNTE = new nt::NetworkTableEntry();
+  //shuffleboardTab = new frc::ShuffleboardTab();
+
+  //shuffleboardTab = shuffleBoard->GetTab("Servo");
+  frc::ShuffleboardTab shuffleboardTab = frc::Shuffleboard::GetTab("Servo");
+  //servoNTE = shuffleboardTab->Add("Servo Angle", 1);
 
   // Crete the GPIO objects
   for(int i = 0; i < LIDAR_COUNT; i++) {
@@ -87,14 +112,30 @@ void Robot::RobotInit() {
  */
 void Robot::RobotPeriodic() {
   static int state=0;
+  //servoNTE = shuffleBoard->add("Servo", 1);
 
-  std::cout << "Measurement= " ;
-  for (int i = 0; i < LIDAR_COUNT; i++) 
-  {
+  servo0->SetAngle(pot->Get());
+  //frc::SendableBuilder::GetEntry("Servo Angle");
+  frc::SmartDashboard::PutNumber("Servo 0 Angle:", servo0->GetAngle());
+  frc::SmartDashboard::PutNumber("Potentiometer Angle:", pot->Get());
+
+  //servoNTE->GetValue();
+
+  frc::SmartDashboard::PutNumber("Lidar Sensor: 0", mLidar[0]->MeasureMM());
+  frc::SmartDashboard::PutNumber("Lidar Sensor: 1", mLidar[1]->MeasureMM());
+  frc::SmartDashboard::PutNumber("Lidar Sensor: 2", mLidar[2]->MeasureMM());
+  frc::SmartDashboard::PutNumber("Lidar Sensor: 3", mLidar[3]->MeasureMM());
+  frc::SmartDashboard::PutNumber("Lidar Sensor: 4", mLidar[4]->MeasureMM());
+  frc::SmartDashboard::PutNumber("Lidar Sensor: 5", mLidar[5]->MeasureMM());
+
+  frc::SmartDashboard::PutBoolean("Line Sensor:", !(lineSensor->Get()));
+//  std::cout << "Measurement= " ;
+//  for (int i = 0; i < LIDAR_COUNT; i++) 
+//  {
 //    std::cout << "Measurement " << i << " " << mLidar[i]->MeasureMM() << std::endl; 
-    printf(" %4d ", mLidar[i]->MeasureMM()); 
-  }
-  std::cout << std::endl;
+//    printf(" %4d ", mLidar[i]->MeasureMM()); 
+//  }
+//  std::cout << std::endl;
 
   //printf(" Measure= %4d\n", mLidar[3]->MeasureMM());
 
@@ -132,9 +173,8 @@ void Robot::RobotPeriodic() {
  */
 void Robot::AutonomousInit() {
   m_autoSelected = m_chooser.GetSelected();
-  // m_autoSelected = SmartDashboard::GetString("Auto Selector",
-  //     kAutoNameDefault);
-  std::cout << "Auto selected: " << m_autoSelected << std::endl;
+  // m_autoSelected = SmartDashboard::GetString("Auto Selector", kAutoNameDefault);
+  //std::cout << "Auto selected: " << m_autoSelected << std::endl;
 
   if (m_autoSelected == kAutoNameCustom) {
     // Custom Auto goes here
